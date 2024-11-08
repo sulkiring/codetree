28de30ad34db7e1e27c029b82297677b4907ed90
sulkiring <154138844+sulkiring@users.noreply.github.com>
2024-11-08 13:41:37 +0900

    Title: 두 개의 동일한 수열, Time: 169ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 241108/두 개의 동일한 수열/two-equal-series.js
@@ -0,0 +1,24 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const n = Number(input[0]);
+const a = input[1].trim().split(' ').map(Number);
+const b = input[2].trim().split(' ').map(Number);
+
+function equal() {
+    for (let i=0; i<n; i++) {
+        if (a[i]!==b[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+a.sort((a,b) => a-b);
+b.sort((a,b) => a-b);
+
+if (equal()) {
+    console.log("Yes");
+} else {
+    console.log("No")
+}
