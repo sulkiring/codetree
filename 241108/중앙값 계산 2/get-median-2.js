cb295dae205a7c80501581cb7e7eb2f88cabb718
sulkiring <154138844+sulkiring@users.noreply.github.com>
2024-11-08 15:53:01 +0900

    Title: 중앙값 계산 2, Time: 189ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 241108/중앙값 계산 2/get-median-2.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const n = Number(input[0]);
+const nums = input[1].split(' ').map(Number);
+
+let result = [];
+
+for (let i=0; i<n; i++) {
+    if (i%2===0) {
+        result.push(nums[Math.floor(i/2)]);
+    }
+}
+
+console.log(result.join(' '))
