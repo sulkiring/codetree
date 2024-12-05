5939a4b781b9348ec6f9d6b6c2e97382b21f9bc2
sulkiring <154138844+sulkiring@users.noreply.github.com>
2024-12-05 16:31:20 +0900

    Title: 연속되는 수 3, Time: 186ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 241205/연속되는 수 3/continuous-number3.js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const n = Number(input[0]);
+const arr = input.slice(1, 1 + n).map(Number)
+
+let ans = 0, cnt = 0;
+for (let i=0; i<n; i++) {
+    if (i>=1 && arr[i] * arr[i-1] > 0 ) {
+        cnt += 1;
+    }
+
+    else {
+        cnt = 1;
+    }
+
+    ans = Math.max(ans, cnt);
+
+}
+
+console.log(ans);
