41161a7e9e66402175ddf7e0b956431b5b240b73
sulkiring <154138844+sulkiring@users.noreply.github.com>
2025-03-17 10:37:48 +0900

    Title: 컨베이어 벨트, Time: 179ms, Memory: 15MB, Status: Wrong Answer - CodeTree

--- 250317/컨베이어 벨트/conveyor-belt.js
+++ 250317/컨베이어 벨트/conveyor-belt.js
@@ -6,17 +6,20 @@ const d = input[2].split(' ').map(Number);
 
 
 for (let i=0; i<t; i++) {
-    let temp = u[n-1]
+    let temp_u = u[n-1]
+    let temp_d = d[n - 1];
 
     for (let j=n-1; j>0; j--) {
         u[j] = u[j-1];
     }
 
+    u[0] = temp_d;
+
     for (let j=n-1; j>0; j--) {
         d[j] = d[j-1];
     }
 
-    d[0] = temp;
+    d[0] = temp_u;
 }
 
 console.log(u.join(' '));
